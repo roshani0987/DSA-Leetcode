b9903f2138ff20c3338f1f6c38e898795d546d8e
Roshani singh <roshani.25b01010200@abes.ac.in>
2026-08-19 22:19:42 +0530

    Time: 76 ms (25.09%), Space: 82.4 MB (46.37%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> current;
+
+    void backtrack(int start, int n, int k) {
+        // If we have k numbers, store the combination
+        if (current.size() == k) {
+            ans.push_back(current);
+            return;
+        }
+
+        // Choose numbers from start to n
+        for (int i = start; i <= n; i++) {
+            current.push_back(i);
+
+            backtrack(i + 1, n, k);
+
+            current.pop_back();
+        }
+    }
+
+    vector<vector<int>> combine(int n, int k) {
+        backtrack(1, n, k);
+        return ans;
+    }
+};
